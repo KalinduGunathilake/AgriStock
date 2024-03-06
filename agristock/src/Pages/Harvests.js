dd9d6de1cefa2e868652c14dbdcae2de3896d3be
Kalindu Gunathilake <kalindugunathilake123@gmail.com>
2024-03-06 14:48:13 +0530

    Harvests Remake

--- agristock/src/Pages/Harvests.js
+++ agristock/src/Pages/Harvests.js
@@ -1,43 +1,38 @@
-import React from 'react'
-import Navbar from '../Components/navbar'
-import '../Styles/harvest.css'
-import Product from '../Components/Product'
+// import React from 'react'
+// import Navbar from '../Components/navbar'
+// import '../Styles/harvest.css'
+// import Product from '../Components/Product'
 
-export default function() {
-  return (
-    <div className='harvest'>
-        <Navbar/>
-       
-       <div className='harvest-Title'>
-          <h1 className='harvestTitle'> Available Harvest:</h1>
-          <h2 className='harvestName'>CARROT</h2>
-          
-          </div>
-          <div className='harvest-container'>
-          <Product ExpireDate={2}  location="Matara,Sri Lanka" image="https://img.freepik.com/free-vector/carrot-colorful-vector-illustration-cartoon_341269-1112.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
-          className='card-img'/>
-          <Product image="https://img.freepik.com/free-vector/fresh-carrot-vegetable_24877-81698.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
-          className='card-img'/>
-          <Product image="https://img.freepik.com/free-vector/hand-drawn-carrot-cartoon-illustration_52683-131161.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
-          className='card-img'/>
-          <Product image="https://img.freepik.com/free-vector/hand-drawn-carrot-cartoon-illustration_23-2150650397.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
-          className='card-img'/>
-          </div>
-          <div className='harvest-container'>
-          <Product image="https://img.freepik.com/free-vector/hand-drawn-carrot-cartoon-illustration_23-2150621292.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
-          className='card-img'/> 
-          <Product image="https://img.freepik.com/free-vector/carrot-root-soil-cartoon-style-isolated_1308-57336.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
-          className='card-img'/>
-          <Product image="https://img.freepik.com/free-vector/hand-drawn-carrot-cartoon-illustration_52683-128109.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
-          className='card-img'/>
-          <Product image="https://img.freepik.com/premium-vector/cartoon-carrot-illustration_193274-1896.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.2.1290981190.1705661824&semt=ais" alt="carrot"
-          className='card-img'/>
-          </div>
-           
-           
+// export default function () {
+// 	return (
+// 		<div className='harvest'>
+// 			<Navbar />
 
-          
-     
-    </div>
-  )
-}
+// 			<div className='harvest-Title'>
+// 				<h1 className='harvestTitle'> Available Harvest:</h1>
+// 				<h2 className='harvestName'>CARROT</h2>
+
+// 			</div>
+// 			<div className='harvest-container'>
+// 				<Product ExpireDate={2} location="Matara,Sri Lanka" image="https://img.freepik.com/free-vector/carrot-colorful-vector-illustration-cartoon_341269-1112.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
+// 					className='card-img' />
+// 				<Product image="https://img.freepik.com/free-vector/fresh-carrot-vegetable_24877-81698.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
+// 					className='card-img' />
+// 				<Product image="https://img.freepik.com/free-vector/hand-drawn-carrot-cartoon-illustration_52683-131161.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
+// 					className='card-img' />
+// 				<Product image="https://img.freepik.com/free-vector/hand-drawn-carrot-cartoon-illustration_23-2150650397.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
+// 					className='card-img' />
+// 			</div>
+// 			<div className='harvest-container'>
+// 				<Product image="https://img.freepik.com/free-vector/hand-drawn-carrot-cartoon-illustration_23-2150621292.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
+// 					className='card-img' />
+// 				<Product image="https://img.freepik.com/free-vector/carrot-root-soil-cartoon-style-isolated_1308-57336.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
+// 					className='card-img' />
+// 				<Product image="https://img.freepik.com/free-vector/hand-drawn-carrot-cartoon-illustration_52683-128109.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.1.1290981190.1705661824&semt=ais" alt="carrot"
+// 					className='card-img' />
+// 				<Product image="https://img.freepik.com/premium-vector/cartoon-carrot-illustration_193274-1896.jpg?size=626&ext=jpg&uid=R134159092&ga=GA1.2.1290981190.1705661824&semt=ais" alt="carrot"
+// 					className='card-img' />
+// 			</div>
+// 		</div>
+// 	)
+// }
